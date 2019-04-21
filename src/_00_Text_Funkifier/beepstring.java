2443695ac33fd7b1ac0f742125077b45d785e80a
beep <samuelsharp4@gmail.com>
2019-04-21 12:41:28 -0700

    Custom Special :stuck_out_tongue: sTrInG

---
+++ src/_00_Text_Funkifier/beepstring.java
@@ -0,0 +1,14 @@
+package _00_Text_Funkifier;
+
+public class beepstring extends SpecialString{
+
+	public beepstring(String s) {
+		super(s);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String funkifyText(String s) {
+		return "Beep";
+	}
+	
+}
